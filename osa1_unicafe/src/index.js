42a9905288b24e1d2a18e3a5f9ccb9aa722886d1
Kristian Harju <harjuko@gmail.com>
2019-05-19 18:13:16 +0100

    osa 1.6. unicafe

---
+++ osa1_unicafe/src/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = (props) => (
+  <p> {props.text} {props.value} </p>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const goodReview = () => {
+    setGood(good + 1)
+  }
+
+  const neutralReview = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const badReview = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>anna palautetta</h1>
+      <Button handleClick={goodReview} text='Hyvä'  />
+      <Button handleClick={neutralReview} text='Neutraali'  />
+      <Button handleClick={badReview} text='Huono'  />
+      <h1>statistiikka</h1>
+      <Display text='Hyvä' value={good} />
+      <Display text='Neutraali' value={neutral} />
+      <Display text='Huono' value={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
