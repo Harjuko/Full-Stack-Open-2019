2e8296289fab21c76f013893bc52bfcc9652fa63
Kristian Harju <harjuko@gmail.com>
2019-05-26 13:12:13 +0100

    Osa2.1. kurssitiedot Step 6

---
+++ osa2/src/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Header = ({course}) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Part = ({name, exercises}) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = ({course}) => {
+  const rows = () =>
+    course.parts.map(part =>
+      <Part key={part.id} name={part.name} exercises={part.exercises} />)
+  return (
+    <div>
+      {rows()}
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
